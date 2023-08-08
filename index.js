229cf214387869a9e7a27dbea6e3c1f46cc45d43
Muhammad Umair Shaikh <64693746+umarshaikh64@users.noreply.github.com>
2023-08-09 04:09:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+// app.use(cors())
+var allowedOrigins = ['https://video-rtc.com/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // https://youtube.com/shorts/UHHEPlIEO6Y?feature=share
 
 
