2c40b2cd6fa58de20c37ba5f429661fdcb50405c
Muhammad Umair Shaikh <64693746+umarshaikh64@users.noreply.github.com>
2023-08-25 01:03:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,9 @@ app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 
 app.use(express.static(path.join(__dirname + "/downloads/")))
 app.get("/", (req, res) => {
