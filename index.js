2cd8bc0ff1ff4d103eefc55d651c1bb338f237e1
Muhammad Umair Shaikh <64693746+umarshaikh64@users.noreply.github.com>
2023-08-25 03:31:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -451,11 +451,7 @@ app.post('/downloadMedia', async (req, res) => {
         ytdl(url, {
             format: type,
             quality: format.map(v => v.itag)
-        }).pipe(fileStream).on("error", (err) => res.status(500).json({
-            status: false,
-            code: 500,
-            error: err
-        }));
+        }).pipe(fileStream);
         fileStream.on("finish", () => fileStream.close(() => {
             res.status(200).json(
                 {
